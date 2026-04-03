2cce343dfe13fb71fe020bef6f178248ddcf9d63
EC2 Default User <ec2-user@ip-172-31-79-43.ec2.internal>
2026-04-03 17:36:23 +0500

    Fix class name to match filename App.java

--- src/main/java/com/saqib/app/App.java
+++ src/main/java/com/saqib/app/App.java
@@ -1,6 +1,6 @@
 package com.example;
 
-public class Hello {
+public class App {
     public static void main(String[] args) {
         System.out.println("Hello World! i have updated my thesis-app folder. this folder was empty before.");
     }
