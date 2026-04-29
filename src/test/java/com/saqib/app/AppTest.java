ae2bb8bedca7280175a957738a04691e402be177
EC2 Default User <ec2-user@ip-172-31-79-43.ec2.internal>
2026-04-29 11:38:02 +0500

    Updated AppTest.java with fixes

--- src/test/java/com/saqib/app/AppTest.java
+++ src/test/java/com/saqib/app/AppTest.java
@@ -1,9 +1,8 @@
-package com.saqib.app;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-public class AppTest {
-    @Test
-    public void testApp() {
-        assertTrue(true);
-    }
+
+class MyClassTest {
+  @Test
+  void test() {
+    // ...
+  }
 }
