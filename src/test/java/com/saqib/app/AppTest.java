19e9a1ccee498d1e35a2d8746f56ea4018167ea4
EC2 Default User <ec2-user@ip-172-31-79-43.ec2.internal>
2026-04-29 12:12:49 +0500

    updated ex file

--- src/test/java/com/saqib/app/AppTest.java
+++ src/test/java/com/saqib/app/AppTest.java
@@ -1,7 +1,11 @@
+package com.saqib.app;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 public class AppTest {
     @Test
     public void testApp() {
         assertTrue(true);
     }
 }
+
 
