7b37dee4a7cdf6d4c3f653322e5959ce1db55e15
EC2 Default User <ec2-user@ip-172-31-79-43.ec2.internal>
2026-04-29 12:49:22 +0500

    Updated App.java with fixes

--- src/main/java/com/saqib/app/App.java
+++ src/main/java/com/saqib/app/App.java
@@ -3,6 +3,6 @@ package com.saqib.app; // This MUST match the folder name
 public class App {
     public static void main(String[] args) {
         // We will keep it simple to ensure the test still works
-        System.out.println("Hello Saqib, Pipeline is working!"); 
+       logger.log("Hello Saqib, Pipeline is working!"); 
     }
 }
